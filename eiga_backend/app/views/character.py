861646c7b0c3a14049f61cb9e435520cbe897080
youkoaona <1242173817@qq.com>
2023-11-24 17:31:05 +0800

    SubjectSearch.vue

--- eiga_backend/app/views/character.py
+++ eiga_backend/app/views/character.py
@@ -3,3 +3,24 @@ from rest_framework.response import Response
 from app.models import Bangumi
 from app.models import CharacterBangumi
 from app.models import Character
+
+
+class CharacterSearch(APIView):
+    def get(self, request):
+        pattern = request.GET.get('pattern')
+        obj_list_data = []
+        print('search character: pattern=' + pattern)
+        try:
+            list = Character.objects.filter(character_name__contains=pattern)
+            print(list)
+            for obj in list:
+                obj_list_data.append({
+                    "id": obj.character_id,
+                    "name": obj.character_name,
+                })
+        except Exception as e:
+            print(e)
+            return Response(1)
+        return Response({
+            'characters': obj_list_data
+        })
---
+++ eiga_frontend/front/src/components/ListItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-card :body-style="{ padding: '0px' }">
+    <img
+        :src="image"
+        class="image"
+    />
+    <div style="padding: 14px">
+      <span>{{type}},{{ name }}</span>
+      <div class="bottom">
+        <el-button text class="button">
+          <router-link :to="'/'+type +'/'+ id">Check</router-link>
+        </el-button>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import http from "../utils/http";
+
+export default {
+  props: ['type','id','name', 'image', 'description'],
+  data() {
+    return {
+      characters: []
+    }
+  },
+  methods: {
+  },
+  name: "ListItem",
+}
+
+</script>
+
+<style>
+.time {
+  font-size: 12px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+</style>
