3ebba3aae927a421c5d17d4996bccdf4694ed3ef
LorenzoFH <gaohf2002@163.com>
2023-11-27 00:30:01 +0800

    完成用户info修改？(没完整测试，因为あおな同学还没弄好)

--- eiga_frontend/front/src/components/UserLogin.vue
+++ eiga_frontend/front/src/components/UserLogin.vue
@@ -1,78 +1,82 @@
 <template>
-    <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" class="ruleForm">
-      <el-form-item label="用户邮箱" prop="pass">
-        <el-input v-model="email" class="inputBar" width autocomplete="off" placeholder="Please input" clearable/>
-      </el-form-item>
-      <el-form-item label="登录密码" prop="checkPass">
-        <el-input v-model="password" class="inputBar" width type="password" autocomplete="off" placeholder="Please input password" show-password/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submitForm(ruleFormRef)">
-          <a href="/" @click.prevent="send_post">login</a>
-        </el-button>
-        <el-button><router-link to="/reg">register</router-link></el-button>
-      </el-form-item>
-    </el-form>
+  <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" class="ruleForm">
+    <el-form-item label="用户邮箱" prop="pass">
+      <el-input v-model="email" class="inputBar" width autocomplete="off" placeholder="Please input" clearable/>
+    </el-form-item>
+    <el-form-item label="登录密码" prop="checkPass">
+      <el-input v-model="password" class="inputBar" width type="password" autocomplete="off"
+                placeholder="Please input password" show-password/>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">
+        <a href="/" @click.prevent="send_post">login</a>
+      </el-button>
+      <el-button>
+        <router-link :to="{name : 'Register'}">register</router-link>
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
-   
-  <script>
-  import http from "../utils/http.ts";
-   
-  export default {
-    name: "Login",
-    data() {
-      return {
-        email: '',
-        password: '',
-        tip: '',
-        exception: '',
-      }
-    },
-    methods: {
-      send_post() {
-        http.post("http://127.0.0.1:8000/user_login/", {
-              email: this.email,
-              password: this.password,
-            },
-        ).then(response => {
-          if (response.data.state == '1') {
-            console.log('登录成功')
-            this.exception = ''
-            this.tip = response.data.tip
-            console.log(response.data.user_id)
-            console.log(response.data.username)
-            console.log(response.data.permission)
-            localStorage.setItem("login_status", true);
-            localStorage.setItem("user_id", response.data.user_id);
-            localStorage.setItem("username", response.data.username);
-            localStorage.setItem("permission", response.data.permission);
-            window.location.href = "/Home"
-          } else {
-            console.log('登录失败')
-            this.exception = response.data.exception
-            this.tip = response.data.tip
-            if (this.exception == "user_not_exist") {
-                alert("用户不存在")
-            } else if (this.exception == "password_wrong") {
-                alert("密码错误")
-            } else {
-                alert("其他错误")
-            }
-          }
-        }).catch(error => {
-          console.log('请求错误!error=', error)
-        })
-      },
+
+<script>
+import http from "../utils/http.ts";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: '',
+      password: '',
+      tip: '',
+      exception: '',
     }
+  },
+  methods: {
+    send_post() {
+      http.post("http://127.0.0.1:8000/user_login/", {
+            email: this.email,
+            password: this.password,
+          },
+      ).then(response => {
+        if (response.data.state === '1') {
+          console.log('登录成功')
+          this.exception = ''
+          this.tip = response.data.tip
+          console.log(response.data.user_id)
+          console.log(response.data.username)
+          console.log(response.data.permission)
+          localStorage.setItem("login_status", true);
+          localStorage.setItem("user_id", response.data.user_id);
+          localStorage.setItem("username", response.data.username);
+          localStorage.setItem("permission", response.data.permission);
+          window.location.href = "/Home"
+        } else {
+          console.log('登录失败')
+          this.exception = response.data.exception
+          this.tip = response.data.tip
+          if (this.exception === "user_not_exist") {
+            alert("用户不存在")
+          } else if (this.exception === "password_wrong") {
+            alert("密码错误")
+          } else {
+            alert("其他错误")
+          }
+        }
+      }).catch(error => {
+        console.log('请求错误!error=', error)
+      })
+    },
   }
-  </script>
-   
-   
-  <style scoped>
-  .inputBar {
-    width: 300px;
-  }
-  span {
-    color: red;
-  }
-  </style>
+}
+</script>
+
+
+<style scoped>
+.inputBar {
+  width: 300px;
+}
+
+span {
+  color: red;
+}
+</style>
